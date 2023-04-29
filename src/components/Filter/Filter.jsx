0d15fc3dc6127570b738c57cf61f5869ef390efc
Oleg833 <oleg.kovtun447@gmail.com>
2023-04-29 19:09:42 +0300

    initial copy

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import css from './Filter.module.css';
+
+const Filter = ({ filter, changeFilterInput }) => (
+    <label className={css.label} >
+        Find Contact By Name
+        <input className={css.input}
+        type="text"
+        name={filter}
+        onChange={changeFilterInput}
+        placeholder="Find contacts by name"
+        />
+    </label>
+);
+
+Filter.propTypes = {
+filter: PropTypes.string.isRequired,
+changeFilterInput: PropTypes.func.isRequired,
+};
+
+export default Filter;
